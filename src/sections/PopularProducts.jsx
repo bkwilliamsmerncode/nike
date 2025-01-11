afeaa64663c3fb4f5960ee086632c6bf89f2e29c
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2025-01-10 21:44:02 -0700

    Started and completed super quality section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -8,18 +8,16 @@ const PopularProducts = () => {
         <h2 className="font-palanquin text-4xl font-bold">
           Our <span className="text-coral-red">Popular</span> Products
         </h2>
-        <p className=" lg:max-w-lg font-montserrat text-slate-gray mt-2">
-          Experiance top-notch quality and style with our sought-after
-          selections. Discover a world of comfort, design, and value
+        <p className="lg:max-w-lg font-montserrat text-slate-gray mt-2">
+          Experience top-notch quality and style with our sought-after
+          selections. Discover a world of comfort, design, and value.
         </p>
       </div>
 
       <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
-        {products.map((product) => {
-       
-            <PopularProductCard key={product.name} {...product} />
-       
-        })}
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
       </div>
     </section>
   );
